7b10c8a9ef8934812c362515b0992b4d2ec9183d
Diego <diferox@gmail.com>
2022-06-11 01:10:33 -0500

    edit .css and .js file

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let computerRPS = ["Rock", "Paper", "Scissors"];
 let computerSelection = "";
 let playerSelection = "";
 let win = 0;
+let lose = 0;
 
 function computerPlay()
 {
@@ -61,17 +62,42 @@ function playRound(playerSelection, computerSelection)
 
 function game (playerSelection, computerSelection)
 {
-  if (playRound(playerSelection, computerSelection) === "You Win!, Rock beats Scissors" || playRound(playerSelection, computerSelection) === "You Win!, Paper beats Rock" || playRound(playerSelection, computerSelection) === "You Win!, Scissors beats Paper")
+  if (playRound(playerSelection, computerSelection) === "There is no winner")
   {
-    return "1";
+    return;
+  }
+  else if (playRound(playerSelection, computerSelection) === "You Win!, Rock beats Scissors" || playRound(playerSelection, computerSelection) === "You Win!, Paper beats Rock" || playRound(playerSelection, computerSelection) === "You Win!, Scissors beats Paper")
+  {
+    win += 1;
+    return win;
   }
   else
   {
-    return "0";
+    lose += 1;
+    return lose;
   }
 }
 
+function text()
+{
+  let answComp = document.getElementsByClassName('answComp');
+  answComp[0].textContent = computerSelection;
 
+  let infoHead = document.getElementsByClassName('infoHead');
+  let info = playRound(playerSelection, computerSelection);
+  infoHead[0].textContent = info;
+
+  //win += parseInt (game(playerSelection, computerSelection));
+  game(playerSelection, computerSelection)
+
+  let scorePlayer = document.getElementsByClassName('scorePlayer');
+  scorePlayer[0].textContent = win;
+  let scoreComp = document.getElementsByClassName('scoreComp');
+  scoreComp[0].textContent = lose;
+  
+}
+
+/*
 for (let i=1; i<=5; i++)
 {
   //Loop while player type the correct answer
@@ -80,10 +106,59 @@ for (let i=1; i<=5; i++)
     playerSelection = prompt("Rock - Paper - Scissors").toLowerCase();  
 
   } while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors");
+  */
 
+  //let content = document.getElementsByClassName('buttonRPS');
+
+
+
+    let btnR = document.getElementsByClassName('buttonRock');
+
+    btnR[0].addEventListener('click', function() 
+    {
+      playerSelection = "rock";
+      computerSelection = computerRPS[computerPlay()];
+
+      let answPlayer = document.getElementsByClassName('answPlayer');
+      answPlayer[0].textContent = "Rock";
+
+      text();
+
+    });
+
+    let btnP = document.getElementsByClassName('buttonPaper');
+
+    btnP[0].addEventListener('click', function() 
+    {
+      playerSelection = "paper";
+      computerSelection = computerRPS[computerPlay()];
+
+      let answPlayer = document.getElementsByClassName('answPlayer');
+      answPlayer[0].textContent = "Paper";
+
+      text();
+
+    });
+
+    let btnS = document.getElementsByClassName('buttonScissors');
+
+    btnS[0].addEventListener('click', function() 
+    {
+      playerSelection = "scissors";
+      computerSelection = computerRPS[computerPlay()];
+
+      let answPlayer = document.getElementsByClassName('answPlayer');
+      answPlayer[0].textContent = "Scissors";
+
+      text();
+
+    });
+
+
+/*
   computerSelection = computerRPS[computerPlay()];
 
-  console.log ("Round " + i);
+  //console.log ("Round " + i);
   console.log ("Player 1 " + playerSelection);
   console.log ("Player 2 " + computerSelection);
 
@@ -92,8 +167,8 @@ for (let i=1; i<=5; i++)
   win += parseInt (game(playerSelection, computerSelection));
 
   console.log ("You win " + win + " round");
-}
-
+//}
+/*
 if (win < 3)
 {
   alert("You Lose!");
@@ -101,4 +176,4 @@ if (win < 3)
 else
 {
   alert ("You Win!");
-}
+}*/
